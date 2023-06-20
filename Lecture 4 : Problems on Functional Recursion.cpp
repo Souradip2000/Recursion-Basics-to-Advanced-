8eb25a7899acf7810075765143a30f618144130e
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-06-20 20:23:29 +0530

    Update Lecture 4 : Problems on Functional Recursion.cpp

--- Lecture 4 : Problems on Functional Recursion.cpp
+++ Lecture 4 : Problems on Functional Recursion.cpp
@@ -20,3 +20,30 @@ int main()
     
     return 0;
 }
+
+
+// Program to check for a palindrome using functional recursion
+
+#include <iostream>
+using namespace std;
+
+bool chk_P(int i, string str, int n)
+{
+    if(i>=n/2)
+        return true;
+    
+    if(str[i]!=str[n-i-1])
+        return false;
+    
+    return chk_P(i+1, str, n);
+}
+
+int main()
+{
+    string str= "MADAM";
+    cout<<str.length()<<endl;
+    bool ans=chk_P(0,str,str.length());
+    cout<<ans;
+    
+    return 0;
+}
