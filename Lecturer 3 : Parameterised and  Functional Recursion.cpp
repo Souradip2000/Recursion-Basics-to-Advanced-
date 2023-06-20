f5e54a6ea028302fd4c56a7419caaf77bf330436
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-06-20 11:10:22 +0530

    Update Lecturer 3 : Parameterised and  Functional Recursion.cpp

--- Lecturer 3 : Parameterised and  Functional Recursion.cpp
+++ Lecturer 3 : Parameterised and  Functional Recursion.cpp
@@ -1,1 +1,20 @@
-//
+// Program to print the sum of the first N numbers using parameterised recursion
+
+#include <iostream>
+using namespace std;
+
+void printSum(int n, int sum)
+{
+    if(n<1)          // base case
+    {
+        cout<<"Sum till N is: "<<sum;
+        return;
+    }
+    printSum(n-1,sum+n);
+}
+
+int main()
+{
+    printSum(3,0);
+    return 0;
+}
