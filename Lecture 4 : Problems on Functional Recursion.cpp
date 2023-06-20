e0635d281866ed521e03c022d51b6812dcac12fc
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-06-20 19:46:53 +0530

    Update Lecture 4 : Problems on Functional Recursion.cpp

--- Lecture 4 : Problems on Functional Recursion.cpp
+++ Lecture 4 : Problems on Functional Recursion.cpp
@@ -1,1 +1,22 @@
-// Program to reverse an array
+// Program to reverse an array using only one pointer by using recursion
+
+#include<iostream>
+using namespace std;
+
+void revArr(int i, int arr[], int n)
+{
+    if(i>=n/2)
+        return;
+    swap(arr[i],arr[n-i-1]);
+    revArr(i+1, arr, n);
+}
+
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    revArr(0, arr, 5);
+    for(int i: arr)
+        cout<<i<<" ";
+    
+    return 0;
+}
