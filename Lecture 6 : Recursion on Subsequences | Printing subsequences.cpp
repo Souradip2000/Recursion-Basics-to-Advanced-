a816130be9ba68e68811fe5349000d5e2ce3852b
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-06-21 22:23:34 +0530

    Update Lecture 6 : Recursion on Subsequences | Printing subsequences.cpp

--- Lecture 6 : Recursion on Subsequences | Printing subsequences.cpp
+++ Lecture 6 : Recursion on Subsequences | Printing subsequences.cpp
@@ -2,4 +2,16 @@
 Q. What is a subsequence?
 Ans : it is a contiguous / non-contiguous sequence which follows the order.
       [ sub-array will always be contiguous ]
+      
+      following order means : [3,1,2] is the array    -     so [3,2,1] will not be a subsequence because 2 comes after 1 in the array
+                                                            whereas [3,2] is a subsequence.
+                                                          
+      for [3,1,2] : { }
+                    3
+                    1
+                    2
+                    3 1
+                    1 2
+                    3 2
+                    3 1 2                                   are all subsequences. { } is a subsequnce too because we've not selected any element and they're in order.
 */
