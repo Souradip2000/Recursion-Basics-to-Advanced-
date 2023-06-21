242e937447aa65a663f08e118b927949c7c51619
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-06-21 21:36:51 +0530

    Update Lecture 5 : Multiple Recursion Calls.cpp

--- Lecture 5 : Multiple Recursion Calls.cpp
+++ Lecture 5 : Multiple Recursion Calls.cpp
@@ -1,1 +1,22 @@
-// Program to print the Fibonacci series
+// Program to print nth Fibonacci number
+
+#include <iostream>
+using namespace std;
+
+int fibo(int n)
+{
+    if(n==0)
+        return 0;
+    else if(n==1)
+        return 1;
+    else
+        return fibo(n-1)+fibo(n-2);
+}
+
+int main()
+{
+    int ans=fibo(5);
+    cout<<ans;
+    
+    return 0;
+}
