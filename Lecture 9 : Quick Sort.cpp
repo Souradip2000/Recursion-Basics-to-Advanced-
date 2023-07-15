46a1e6ae77fef0e9befc5d1f6fc848734df7c7d2
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-07-15 18:58:54 +0530

    Update Lecture 9 : Quick Sort.cpp

--- Lecture 9 : Quick Sort.cpp
+++ Lecture 9 : Quick Sort.cpp
@@ -1,3 +1,20 @@
 /*
 Does the sorting both in acending order as well as in descending order
+
+Why Quick sort?
+A : It has a time complexity similar to merge sort i.e. O(nlogn)
+    but it has constant space complexity O(1) compared to linear space complexity of merge sort.
+
+    Thus quick sort is slightly better than merge sort.
+    Although there are some problems that can be solved only with the help of merge sort.
+
+
+Steps:-
+    1. Pick up a pivot element
+    2. Place it in its correct position
+    3. Put the remaining smaller elements on the left of the pivot and the larger ones on its right.
+
+Rules to select the pivot element:
+    Any element can be selected as the pivot element.
+    
 */
